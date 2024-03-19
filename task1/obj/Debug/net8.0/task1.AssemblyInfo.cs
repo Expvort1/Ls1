d670f07b2a34e266e49d3049aa77cf7acbe6b435
Expvort1 <twidroyd1525@gmail.com>
2024-03-19 09:34:22 +0300

    add task5 поиск числа N

--- task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e83ec2632a4eb392cb56351c39e806d9f2f8bbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b055aeffdc4a0bf659334e4f6ca633f75785d816")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
